15b3328ec3458d0ead94f4dd0848451dcbb3f757
ropert911 <254323626@qq.com>
2019-12-31 14:59:29 +0800

    原始数据解析成基本数据结构并写入数据库

--- src/main/java/com/xq/secser/secser/InitProject.java
+++ src/main/java/com/xq/secser/secser/InitProject.java
@@ -1,12 +1,7 @@
 package com.xq.secser.secser;
 
-import com.xq.secser.secser.model.FundTypeEnum;
-import com.xq.secser.secser.pojo.po.FoundPo;
-import com.xq.secser.secser.pojo.po.FundDownPo;
-import com.xq.secser.secser.pojo.po.IFund;
-import com.xq.secser.secser.pojo.po.IFundDown;
 import com.xq.secser.secser.service.FundInitService;
-import com.xq.secser.secser.service.FundService;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import java.io.Reader;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author sk-qianxiao
