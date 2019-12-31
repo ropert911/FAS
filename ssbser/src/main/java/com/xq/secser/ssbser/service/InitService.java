97ed2a1f934850162a89119f79042eeeadb4eb0a
ropert911 <254323626@qq.com>
2019-12-31 20:18:10 +0800

    代码结构调整

--- ssbser/src/main/java/com/xq/secser/ssbser/service/InitService.java
+++ ssbser/src/main/java/com/xq/secser/ssbser/service/InitService.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,38 +19,48 @@ import java.util.List;
  * @date 2019/12/31
  */
 @Service
-public class InitService {
+public class InitService implements ApplicationRunner {
     private static Logger logger = LoggerFactory.getLogger(InitService.class);
 
     @Autowired
     FundProvider fundProvider;
     @Autowired
     CompanyProvider companyProvider;
-
     @Autowired
     SqlSessionFactory sqlSessionFactory;
 
+    @Value("${com.xq.secser.download.fund}")
+    private boolean bDowloadFound;
 
-    public void initFoundData(boolean reInit) {
-        if (!reInit) {
-            return;
+    @Value("${com.xq.secser.download.company}")
+    private boolean bDowloadCompany;
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+        //TODO:添加时间统计
+        //下载found数据
+        if (bDowloadFound) {
+            fundProvider.initFoundData();
         }
 
-        fundProvider.initFoundData();
-    }
-
-    public void initCompany(boolean initCompany) {
-        if (!initCompany) {
-            return;
+        //下载com数据
+        if (bDowloadCompany) {
+            companyProvider.initCompany();
         }
 
-        companyProvider.initCompany();
+        //把foud原始数据解析到数据结构表里
+        parseFund();
+
+        //解析公司数据
+        parseCompany();
     }
 
 
     public void parseFund() {
+        List<FoundPo> foundPoList = fundProvider.parseFund();
+
         try (SqlSession session = sqlSessionFactory.openSession(true)) {
-            List<FoundPo> foundPoList = fundProvider.parseFund();
             IFund iFund = session.getMapper(IFund.class);
             iFund.insertFundBatch(foundPoList);
         } finally {
